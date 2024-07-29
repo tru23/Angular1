6f91c98a5b8675a927ea670fde91e00b98d84f8a
Trupti Hande <truptimhande81@gmail.com>
2024-07-30 00:36:31 +0530

    after form

--- src/app/screens/onboarding/components/detail-form/detail-form.component.ts
+++ src/app/screens/onboarding/components/detail-form/detail-form.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-detail-form',
@@ -10,12 +10,28 @@ export class DetailFormComponent {
 
   username:string='';
 
-  restroDetails=new FormGroup({
-    name:new FormControl<string>(''),
-    owner:new FormControl<string>('')
+  // restroDetails=new FormGroup({
+  //   name:new FormControl<string>(''),
+  //   owner:new FormControl<string>(''),
+  //   address_street:new FormControl<string>(''),
+  //   address_City:new FormControl<string>(''),
+  //   address_pin:new FormControl<number | null>(null),
+  //   contact:new FormControl<number | null>(null)
+  // })
+
+  restroDetails=this.formBuilder.group({
+    name:['',Validators.required],   //or new FormControl<string>(''),
+    owner:['',Validators.required],
+    contact:['',Validators.required],
+    address:this.formBuilder.group({
+      address_street:new FormControl<string>('',Validators.required),
+      address_City:new FormControl<string>('',Validators.required),
+      address_pin:new FormControl<number | null>(null,Validators.required)
+      
+    })
   })
 
-  constructor() {
+  constructor(private formBuilder:FormBuilder) {
 
     this.username = 'Trupti';
   }
@@ -25,8 +41,17 @@ export class DetailFormComponent {
   displayName(): string {
     return 'John';
   }
-  displayRestroName(){
-    console.log(this.restroDetails)
+  displayRestroDetails(){
+
+    console.log(this.restroDetails.value.name);
+    console.log(this.restroDetails.value.owner);
+    console.log(this.restroDetails.value.contact);
+
+    const address = this.restroDetails.value.address;
+    console.log(address?.address_street);
+    console.log(address?.address_City);
+    console.log(address?.address_pin);
+
 
   }
 
